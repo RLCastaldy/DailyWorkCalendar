884a136e1e08d9c5fb4bc6a9d9d3fe0143addff8
RLCastaldy <rlcastaldy@gmail.com>
2024-01-25 20:54:48 -0600

    cleaned up code and removed instruction text

--- assets/script.js
+++ assets/script.js
@@ -242,30 +242,3 @@ if (currentHour >= 9 && currentHour < 10) {
      console.log(textFive);
    localStorage.setItem('5-To-Do', textFive);
    });
-
-// // Get references to the textarea and the save button
-// const textarea = document.getElementById('myTextarea');
-// const saveButton = document.getElementById('saveButton');
-
-// // Add an event listener to the save button
-// saveButton.addEventListener('click', function() {
-//   // Get the current value of the textarea
-//   const textareaValue = textarea.value;
-
-//   // Store the value in localStorage
-//   localStorage.setItem('savedText', textareaValue);
-
-//   // Provide feedback to the user
-//   alert('Text saved successfully!');
-// });
-
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
